a069bc8855e55cca53e39618c6b8a0b431dbcf82
gerardobecerra1 <42364794+gerardobecerra1@users.noreply.github.com>
2018-08-16 20:09:02 -0500

    Add files via upload
    problema 3 (tarea)

---
+++ Tarea 3/5240.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	int i, NN;//NN=Numero Natural
+	 cout << "Ingrese un numero: ";
+	 cin >> NN;
+    for (i = 1;i <= NN;i++) {
+		cout << NN << " + " << i << " = " << NN + i << endl;
+	}
+		
+		 system("pause");
+
+}
